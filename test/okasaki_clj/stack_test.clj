3e4e9951493d4072148b8f2bd54b323da5801b83
jane-olszewska <jane.olszewska@duedil.com>
2018-06-06 10:17:08 +0100

    Tests cleaned up a bit

--- test/okasaki_clj/stack_test.clj
+++ test/okasaki_clj/stack_test.clj
@@ -2,8 +2,8 @@
   (:require [clojure.test :refer :all]
             [okasaki-clj.stack :refer :all]))
 
-(deftest vector-test
-  (testing "Vector based implementation"
+(deftest vector-stack-test
+  (testing "Vector based Stack implementation"
     (are [res test] (= res test)
                     true (empty? [])
                     false (empty? [1])
@@ -13,8 +13,8 @@
                     [1] (tail [1 2])
                     [1 2] (cons [1] 2))))
 
-(deftest cons-test
-  (testing "Custom Cons type based implementation"
+(deftest cons-stack-test
+  (testing "Custom Cons type based Stack implementation"
     (are [res test] (= res test)
                     ; Stack protocol implemented
                     true (empty? (cons-stack))
@@ -24,9 +24,13 @@
                     ; ...also able to check Cons for equality
                     (cons-stack) (tail (cons-stack 1))
                     (cons-stack 1) (tail (cons-stack 1 2))
-                    (cons-stack 1 2) (cons (cons-stack 1) 2)
-                    ; ...and comparisons work, too
+                    (cons-stack 1 2) (cons (cons-stack 1) 2))))
+
+(deftest cons-stack-comparison
+  (testing "Comparison of Cons-based Stacks"
+    (are [res test] (= res test)
                     0 (compare (cons-stack 1) (cons-stack 1))
                     1 (compare (cons-stack 1 2) (cons-stack 1))
-                    -1 (compare (cons-stack 1) (cons-stack 1 2)))))
-
+                    -1 (compare (cons-stack 1) (cons-stack 1 2))
+                    1 (compare (cons-stack 1 2) (cons-stack 1 1))
+                    -1 (compare (cons-stack 1 2) (cons-stack 1 3)))))
