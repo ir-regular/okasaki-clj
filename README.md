37b9b66b9de42e79eb1342ad9a4ec206d103f075
jane-olszewska <jane.olszewska@duedil.com>
2018-06-06 09:28:37 +0100

    Initial commit: basic project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+An experiment in implementing examples and exercises
+from Chris Okasaki's Purely Functional Data Structures
+in pure Clojure.
---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject okasaki-clj "0.1.0"
+  :description "A Clojure reimplementation of examples and exercises from Okasaki's Purely Functional Data Structures"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
