42ba295ee09905e2b091dfa78e883e020c1e7f09
jane-olszewska <jane.olszewska@duedil.com>
2018-06-11 23:49:27 +0100

    Exercise 2.4: loop-recur, exception-interrupted version of insert

--- src/okasaki_clj/set.clj
+++ src/okasaki_clj/set.clj
@@ -20,22 +20,25 @@
           :else (recur (.-right set) (.-value set))))))
   (insert [set x]
     {:pre [(instance? Comparable x)]}
-    (if (empty? set)
-      (UnbalancedSet. x nil nil)
-      (try
-        ((fn insert' [set x]
-           (if (nil? set)
-             (UnbalancedSet. x nil nil)
-             (let [value (.value set) left (.left set) right (.right set)]
-               (cond
-                 (< x value) (UnbalancedSet. value (insert' left x) right)
-                 (> x value) (UnbalancedSet. value left (insert' right x))
-                 ; guard against cloning a path of the tree leading to the existing element
-                 :else (throw (UnsupportedOperationException. "Inserting a duplicate element"))))))
-          set x)
-        (catch UnsupportedOperationException e
-          ; in case of a duplicate element insert attempt, return the existing collection instead
-          set))))
+    (try
+      (loop [set (if (empty? set) nil set)
+             maybeX nil
+             fs '()]
+        (cond
+          (nil? set)
+          (if (= x maybeX)
+            ; guard against cloning a path of the tree leading to the existing element
+            (throw (UnsupportedOperationException. "Inserting a duplicate element"))
+            (reduce #(%2 %1) (UnbalancedSet. x nil nil) fs))
+
+          (< x (.-value set))
+          (recur (.-left set) maybeX (conj fs #(UnbalancedSet. (.-value set) % (.-right set))))
+
+          :else
+          (recur (.-right set) (.-value set) (conj fs #(UnbalancedSet. (.-value set) (.-left set) %)))))
+      (catch UnsupportedOperationException e
+        ; in case of a duplicate element insert attempt, return the existing collection instead
+        set)))
   Object
   (equals [this that]
     (and (= (instance? UnbalancedSet that))
