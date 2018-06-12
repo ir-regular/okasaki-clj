f4853668b48a16f4d06da19437c5390bc42fbe0a
jane-olszewska <jane.olszewska@duedil.com>
2018-06-12 21:02:14 +0100

    Exercise 2.5.b: create a balanced binary tree, saving space by sharing nodes

--- src/okasaki_clj/tree.clj
+++ src/okasaki_clj/tree.clj
@@ -2,8 +2,32 @@
 
 (defrecord Tree [value left right])
 
-(defn complete [x d]
+(defn complete
+  "Generate a complete binary tree of depth d, every node containing value x"
+  [x d]
   (when (not (and (integer? d) (pos? d)))
     (throw (IllegalArgumentException. "Tree depth should be a positive integer")))
   (first (take 1 (drop (dec d)
                        (iterate #(Tree. x % %) (->Tree x nil nil))))))
+
+; for an n-element tree, given m=((n-1) div 2)
+;   - if n is odd, tree has two subtrees, both with m elements:
+;     (x, balanced(x, m), balanced(x, m))
+;   - if n is even, tree has two subtrees, one with m elements, one with m+1 elements:
+;     (x, balanced(x, m), (x, balanced(x, m) nil))
+; therefore to build the tree bottom-up, you need to generate a sequence of:
+;   n, ((n-1) div 2), ... 0
+
+(defn balanced
+  "Generate a balanced binary tree of n nodes, all containing value x"
+  [x n]
+  (when (not (pos? n))
+    (throw (IllegalArgumentException. "Tree size should be a positive integer")))
+  (loop [[m & ms] (reverse (take-while pos? (iterate #(quot (dec %) 2) n)))
+         base nil]
+    ; tests (predicate? m)
+    (condp #(%1 %2) m
+      nil? base
+      odd? (recur ms (->Tree x base base))
+      ; :else
+      (recur ms (->Tree x (->Tree x base nil) base)))))
--- test/okasaki_clj/tree_test.clj
+++ test/okasaki_clj/tree_test.clj
@@ -11,3 +11,14 @@
     (is (= (->Tree :x (->Tree :x nil nil) (->Tree :x nil nil)) (complete :x 2)))
     ; left and right branches of a node contain the same object
     (is (let [t (complete :x 2)] (identical? (:left t) (:right t))))))
+
+(deftest build-balanced-tree
+  (testing "Building balanced tree"
+    ; can't provide a non-positive int size
+    (is (thrown? IllegalArgumentException (balanced :x 0)))
+    ; builds a complete tree as expected
+    (is (= (->Tree :x nil nil) (balanced :x 1)))
+    (is (= (->Tree :x (->Tree :x nil nil) nil) (balanced :x 2)))
+    (is (= (complete :x 2) (balanced :x 3)))
+    ; left and right branches of an odd-count tree contain the same object
+    (is (let [t (balanced :x 7)] (identical? (:left t) (:right t))))))
